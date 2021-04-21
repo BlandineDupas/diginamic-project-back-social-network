a9889c7bccad70441302328d499ba62dcf51403d
BlandineDupas <blandine.dupas@sfr.fr>
2021-04-21 22:13:17 +0200

    check token for every request except when not needed

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const Sequelize = require('sequelize');
-const cors = require('cors')
+const cors = require('cors');
+const jwtMiddelware = require('express-jwt');
 
 const app = express();
 const sequelize= new Sequelize('sqlite:database.db');
 
-const SECRET = process.env.SECRET || 'xxx';
+const SECRET = process.env.SECRET || 'secret';
 const PORT = process.env.PORT || 8000
 
 const corsOptions = {
@@ -19,23 +20,39 @@ app.use(express.json());
 const { userRouter } = require('./user/router');
 const { messageRouter } = require('./message/router');
 const { commentRouter } = require('./comment/router');
+const { inviteRouter } = require('./invite/router');
 
 // Models
 const { userModel } = require('./user/model');
 const { messageModel } = require('./message/model');
 const { commentModel } = require('./comment/model');
+const { inviteModel } = require('./invite/model');
 
 const USER = userModel(sequelize);
 const MESSAGE = messageModel(sequelize);
 const COMMENT = commentModel(sequelize);
+const INVITE = inviteModel(sequelize)
 
 const { joinTables } = require('./db-setup'); // {} pour recevoir une fonction et non un objet
 joinTables(sequelize);
 
+/**
+ * Ce middeware valide le JWT et rajoute un attribut user à la requete.
+ * Cet attribut contient la payload du jeton
+ */
+ app.use(
+    jwtMiddelware({ secret: SECRET, algorithms: ['HS256'] }).unless({
+        path: [
+            '/api/login',
+            { url: '/api/user', methods: ['POST'] }
+        ],
+    })
+  );
+
 app.use('/api', userRouter(USER, sequelize, SECRET)); // concatène /api avec les routes du router
 app.use('/api', messageRouter(MESSAGE, sequelize, SECRET));
 app.use('/api', commentRouter(COMMENT, sequelize, SECRET));
-
+app.use('/api', inviteRouter(INVITE, sequelize, SECRET));
 
 sequelize
     .sync()
